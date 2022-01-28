82727361e164adac0259966bdb4cc4895c6d1eae
Sierra Cornelius <83981022+SierraSky7@users.noreply.github.com>
2022-01-28 13:50:47 -0700

    Update feedback (sc140820@umconnect.umt.edu).php

--- feedback (sc140820@umconnect.umt.edu).php
+++ feedback (sc140820@umconnect.umt.edu).php
@@ -4,9 +4,6 @@
 <body>
 <h1>Submitted Form Data</h1>
 <h2>Your input was received as:</h2>
-<div class="w3-container w3-large w3-border" style="word-wrap:break-word">
-firstname=John&lastname=Doe&nbsp;</div>
-
 <p>The server has processed your input and returned this answer.</p>
 </body>
 </html>
