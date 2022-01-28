cc0182371b76bc6029c733305973f2a1937d8cce
Sierra Cornelius <83981022+SierraSky7@users.noreply.github.com>
2022-01-28 13:22:09 -0700

    troubleshooting php

---
+++ feedback (sc140820@umconnect.umt.edu).php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<body>
+<h1>Submitted Form Data</h1>
+<h2>Your input was received as:</h2>
+<div class="w3-container w3-large w3-border" style="word-wrap:break-word">
+firstname=John&lastname=Doe&nbsp;</div>
+
+<p>The server has processed your input and returned this answer.</p>
+</body>
+</html>
