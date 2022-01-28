31f6b55928706119c485e36b0eff8f47b1c47a34
Sierra Cornelius <83981022+SierraSky7@users.noreply.github.com>
2022-01-28 13:06:34 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,7 +2,12 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+  <h1>Submitted Form Data</h1>
+  <h2>Your input was received as:</h2>
+  <div class="w3-container w3-large w3-border" style="word-wrap:break-word">
+  firstname=John&amp;lastname=Doe&nbsp;</div>
+
+  <p>The server has processed your input and returned this answer.</p>
 
 <?php
 echo "Hello World!";
